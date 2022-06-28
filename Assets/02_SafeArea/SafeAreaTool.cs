71037b52cfdcf54c28e928b0515576de65cc1157
mhoow6 <mhoow6@gmail.com>
2022-06-29 02:36:41 +0900

    [2022-06-29] SafeArea

---
+++ Assets/02_SafeArea/SafeAreaTool.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace Mhoow6.SafeArea
+{
+    public abstract class SafeAreaTool : MonoBehaviour
+    {
+        [Tooltip("Device Simulator 사용해도 TRUE")]
+        public bool IsInMoblieDevice;
+
+        protected RectTransform rectTransform;
+        protected Canvas spaceOverlayCanavs;
+
+        protected void Awake()
+        {
+            if (!IsInMoblieDevice)
+            {
+                enabled = false;
+                return;
+            }
+
+            rectTransform = GetComponent<RectTransform>();
+
+            if (TryGetScreenOverlayCanvas(out var canvas))
+                spaceOverlayCanavs = canvas;
+            else
+            {
+                Canvas[] canvaslist = FindObjectsOfType<Canvas>();
+                spaceOverlayCanavs = canvaslist.First(canvas =>
+                {
+                    if (canvas.renderMode == RenderMode.ScreenSpaceOverlay)
+                        return true;
+                    return false;
+                });
+            }
+        }
+
+        bool TryGetScreenOverlayCanvas(out Canvas result)
+        {
+            Canvas find = null;
+            result = find;
+
+            return false;
+        }
+
+        protected virtual void OnAwake() { }
+    }
+}
+
